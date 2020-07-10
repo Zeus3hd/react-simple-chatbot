abf57eb48f05949e6c2320e97bb476f82e08c197
Ahd <dafuq.ahd@gmail.com>
2020-07-10 06:18:17 +0300

    set display none to image container in case user is true

--- lib/steps_components/text/ImageContainer.jsx
+++ lib/steps_components/text/ImageContainer.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const ImageContainer = styled.div`
-  display: inline-block;
+  display: ${({ user }) => (user ? 'none' : 'inline-block')};
   order: ${props => (props.user ? '1' : '0')};
   padding: 6px;
+  margin-right: 2rem;
 `;
 
 export default ImageContainer;
